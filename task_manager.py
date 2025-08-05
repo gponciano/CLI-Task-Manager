8a6279e28f20c41f34d1ae9e803d862de65e1ae3
gponciano <gab_bash@hotmail.com>
2025-08-05 20:04:55 +1000

    creating menu for tasks

---
+++ task_manager.py
@@ -0,0 +1,7 @@
+print("\n Menu to for managing tasks/to-do-list")
+print("1. Add a task")
+print("2. See tasks")
+print("3. Update a task")
+print("4. Mark task as completed")
+print("5. Delete complete task(s)")
+print("6. Exit")
