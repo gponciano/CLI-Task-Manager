5eb008ef5e5924303446593a1a31b233d756a49a
gponciano <gab_bash@hotmail.com>
2025-08-05 22:05:56 +1000

    Adding 'adding task' function and 'see tasks' function

--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,25 @@
+# Task definition:
+# Task: name of the task
+# Completed: () (x)
+
+
+def adding_task(tasks, task_name):
+
+    task = {"name": task_name, "completed": False}
+    tasks.append(task)
+    print(f"Task '{task_name}' has been added")
+    return
+
+def see_tasks(tasks):
+    print("\n See your current to-do list:")
+    for index, task in enumerate(tasks, start =1):
+        status = "✔" if task["completed"] else ""
+        task_name = task["name"]
+        print(f"{index}. [{status}], {task_name}")
+    return
+
+tasks = []
+
 while True:
     print("\n Menu to for managing tasks/to-do-list")
     print("1. Add a task")
@@ -9,5 +31,10 @@ while True:
 
     user_input = input("Please choose an item: ")
 
-    if user_input == "6":
+    if user_input == "1":
+        task_name = input("Please add your task:")
+        adding_task(tasks, task_name)
+    elif user_input == "2":
+        see_tasks(tasks)
+    elif user_input == "6":
         break
