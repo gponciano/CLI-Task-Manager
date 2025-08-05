d947184b9643851917d7e9d8b7183d70db131f01
gponciano <gab_bash@hotmail.com>
2025-08-05 20:07:18 +1000

    Adding while loop and kill switch to exit menu

--- task_manager.py
+++ task_manager.py
@@ -1,7 +1,13 @@
-print("\n Menu to for managing tasks/to-do-list")
-print("1. Add a task")
-print("2. See tasks")
-print("3. Update a task")
-print("4. Mark task as completed")
-print("5. Delete complete task(s)")
-print("6. Exit")
+while True:
+    print("\n Menu to for managing tasks/to-do-list")
+    print("1. Add a task")
+    print("2. See tasks")
+    print("3. Update a task")
+    print("4. Mark task as completed")
+    print("5. Delete complete task(s)")
+    print("6. Exit")
+
+    user_input = input("Please choose an item: ")
+
+    if user_input == "6":
+        break
